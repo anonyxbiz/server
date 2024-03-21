ecd0f0f42f0faef16cede4fc9dcc71e3b9616723
anonyxbiz <160992637+anonyxbiz@users.noreply.github.com>
2024-03-21 06:15:25 +0300

    Update ai.py

--- ai.py
+++ ai.py
@@ -10,3 +10,12 @@ from IPython.display import Markdown
 def to_markdown(text):
   text = text.replace('•', '  *')
   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+# Or use `os.getenv('GOOGLE_API_KEY')` to fetch an environment variable.
+GOOGLE_API_KEY="AIzaSyA2Wuzo0_4IkhY6SMzm0wpPU3N2OyU7Y3Y"
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+for m in genai.list_models():
+  if 'generateContent' in m.supported_generation_methods:
+    print(m.name)
